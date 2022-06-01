bb6a5ed7a78f39dc939738029118df51b204b0e1
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-06-01 17:35:20 -0300

    Create Exercicio 2.py

---
+++ Exercicios/Exercicio 2.py
@@ -0,0 +1,10 @@
+'''Escreva uma expressão para determinar se uma pessoa deve ou não
+pagar imposto. Considere que pagam imposto pessoas cujo salário é maior que
+R$ 1.200,00.'''
+
+salario = input('Olá vamos ver se você tem que pagar impostos? Digite seu salário: ')
+
+if salario <= 1200:
+      print('Não você não precisa pagar imposto!')
+else:
+    print('Sim você tem que pagar imposto!')
