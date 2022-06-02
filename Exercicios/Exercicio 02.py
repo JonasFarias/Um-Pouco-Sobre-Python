b50529d4cb1c87734c78f9560db2ab610098413f
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-06-02 16:47:51 -0300

    Update Exercicio 02.py

--- Exercicios/Exercicio 02.py
+++ Exercicios/Exercicio 02.py
@@ -2,9 +2,9 @@
 pagar imposto. Considere que pagam imposto pessoas cujo salário é maior que
 R$ 1.200,00.'''
 
-salario = input('Olá vamos ver se você tem que pagar impostos? Digite seu salário: ')
+salario = int(input('Olá vamos ver se você tem que pagar impostos? Digite seu salário: '))
 
 if salario <= 1200:
-      print('Não você não precisa pagar imposto!')
+    print('Não você não precisa pagar imposto!')
 else:
     print('Sim você tem que pagar imposto!')
