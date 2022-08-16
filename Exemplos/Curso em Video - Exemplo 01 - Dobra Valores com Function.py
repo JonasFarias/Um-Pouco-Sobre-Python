1ecf146ea1dce26b19fea646f5e3bca9a41b6e84
Jonas Farias <jhonas.jr@hotmail.com>
2022-08-15 22:08:54 -0300

    .

---
+++ Exemplos/Curso em Video - Exemplo 01 - Dobra Valores com Function.py
@@ -0,0 +1,10 @@
+def dobra_valores(lst):
+    posicao = 0
+    while posicao <len(lst):
+        lst[posicao] *= 2
+        posicao +=1
+
+
+valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dobra_valores(valores)
+print(f'{valores}')
