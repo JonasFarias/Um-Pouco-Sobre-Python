a9efb774ac25c5d7b5cdbee095a5a10ccc78bcca
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2022-09-11 15:26:35 -0300

    .

--- Exemplos/classes pokemon.py
+++ Exemplos/classes pokemon.py
@@ -1,7 +1,13 @@
 class Pokemon(): #Criamos a classe pokemon
-    def __int__(self, nome, tipo, level): #Inicializa os atributos, nome, tipo e level
-        self.name
-        self.tipo
+    def __int__(self, nome, tipo, level=1): #Inicializa os atributos, nome, tipo e level
+        nome = self.nome
+        tipo = self.tipo
+    def atacar(self):
+        pass
+
+
+
+
 
 
 
